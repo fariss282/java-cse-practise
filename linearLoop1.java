ee7d701c04e655d3f108a11094bbebe1417978b0
fariss282 <hamidyousuf131@gmail.com>
2026-07-29 14:59:03 +0600

    linear loop 1

---
+++ linearLoop1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class linearLoop1 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+
+     Scanner sc = new Scanner(System.in);
+            int n=sc.nextInt();
+            for (int i=0; i<=n; i++){
+                if(i%5==0 && i%3 !=0){
+                    System.out.println(i);
+                }
+                }
+        }
+}
+
+
+    
+
